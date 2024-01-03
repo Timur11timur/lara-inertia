482ef802434255742c77dc2874fd558e0110bb88
Timur Fralik <timur11timur@gmail.com>
2024-01-03 23:30:20 +0200

    Add passing data

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Route::get('/', function () {
+//    return inertia('Welcome');
+//});
+
 Route::get('/', function () {
-    return inertia('Welcome');
+    return Inertia::render('Home', [
+        'name' => 'Some Name',
+        'frameworks' => ['Laravel', 'Vue', 'Inertia'],
+    ]);
 });
